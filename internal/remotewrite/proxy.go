c79b632e0bead1bbda33d1cd986f471fc0d5e685
marcolan018 <llan@redhat.com>
2022-03-09 00:57:32 -0600

    error log handle to avoid log flooding when one or more remote endpoints not working Signed-off-by: Marco llan@redhat.com

--- internal/remotewrite/proxy.go
+++ internal/remotewrite/proxy.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
-	promconfig "github.com/prometheus/common/config"
 )
 
 const (
@@ -23,7 +22,7 @@ type Endpoint struct {
 	Name string `yaml:"name"`
 	URL  string `yaml:"url"`
 	// +optional
-	ClientConfig *promconfig.HTTPClientConfig `yaml:"http_client_config,omitempty"`
+	ClientConfig *HTTPClientConfig `yaml:"http_client_config,omitempty"`
 }
 
 var (
@@ -57,41 +56,68 @@ func remoteWrite(write *url.URL, endpoints []Endpoint, logger log.Logger) http.H
 		}
 
 		rlogger := log.With(logger, "request", middleware.GetReqID(r.Context()))
-		for _, endpoint := range endpoints {
+		for i, endpoint := range endpoints {
 			var client *http.Client
 			var err error
 			if endpoint.ClientConfig == nil {
 				client = &http.Client{}
 			} else {
-				client, err = promconfig.NewClientFromConfig(*endpoint.ClientConfig, endpoint.Name, true)
+				client, err = NewClientFromConfig(*endpoint.ClientConfig, endpoint.Name, false)
 				if err != nil {
-					level.Error(rlogger).Log("failed to create a new HTTP client", "err", err)
+					//level.Error(rlogger).Log("msg", "failed to create a new HTTP client", "err", err)
+					LogChannels[i] <- logMessage{
+						messageKey: "failed to create a new HTTP client",
+						keyvals: []interface{}{
+							"msg", "failed to create a new HTTP client", "err", err,
+						}}
 				}
 			}
 
 			req, err := http.NewRequest(http.MethodPost, endpoint.URL, bytes.NewReader(body))
 			req.Header = r.Header
 			if err != nil {
-				level.Error(rlogger).Log("msg", "Failed to create the forward request", "err", err, "url", endpoint.URL)
+				//level.Error(rlogger).Log("msg", "Failed to create the forward request", "err", err, "url", endpoint.URL)
+				LogChannels[i] <- logMessage{
+					messageKey: "Failed to create the forward request",
+					keyvals: []interface{}{
+						"msg", "Failed to create the forward request", "err", err,
+					}}
 			} else {
 				ep := endpoint
+				j := i
 				go func() {
 					resp, err := client.Do(req)
 					if err != nil {
 						remotewriteRequests.With(prometheus.Labels{"code": "<error>", "name": ep.Name}).Inc()
-						level.Error(rlogger).Log("msg", "Failed to send request to the server", "err", err)
+						//level.Error(rlogger).Log("msg", "Failed to send request to the server", "err", err)
+						LogChannels[j] <- logMessage{
+							messageKey: "Failed to send request to the server",
+							keyvals: []interface{}{
+								"msg", "Failed to send request to the server", "err", err,
+							}}
 					} else {
 						defer resp.Body.Close()
 						remotewriteRequests.With(prometheus.Labels{"code": strconv.Itoa(resp.StatusCode), "name": ep.Name}).Inc()
 						if resp.StatusCode >= 300 || resp.StatusCode < 200 {
 							responseBody, err := ioutil.ReadAll(resp.Body)
 							if err != nil {
-								level.Error(rlogger).Log("msg", "Failed to read response of the forward request", "err", err, "return code", resp.Status, "url", ep.URL)
+								//level.Error(rlogger).Log("msg", "Failed to read response of the forward request", "err", err, "return code", resp.Status, "url", ep.URL)
+								LogChannels[j] <- logMessage{
+									messageKey: "Failed to forward metrics" + resp.Status,
+									keyvals: []interface{}{
+										"msg", "Failed to forward metrics", "return code", resp.Status, "url", ep.URL,
+									}}
 							} else {
-								level.Error(rlogger).Log("msg", "Failed to forward metrics", "return code", resp.Status, "response", string(responseBody), "url", ep.URL)
+								LogChannels[j] <- logMessage{
+									messageKey: "Failed to forward metrics" + resp.Status,
+									keyvals: []interface{}{
+										"msg", "Failed to forward metrics", "return code", resp.Status, "response", string(responseBody), "url", ep.URL}}
 							}
 						} else {
-							level.Debug(rlogger).Log("msg", "Metrics forwarded successfully", "url", ep.URL)
+							level.Debug(rlogger).Log("msg", successWrite, "url", ep.URL)
+							LogChannels[j] <- logMessage{
+								messageKey: successWrite,
+							}
 						}
 					}
 				}()
@@ -115,6 +141,8 @@ func Proxy(write *url.URL, endpoints []Endpoint, logger log.Logger, r *prometheu
 			Name: THANOS_ENDPOINT_NAME,
 		})
 	}
+
+	InitChannels(logger, len(endpoints))
 
 	return remoteWrite(write, endpoints, logger)
 }
