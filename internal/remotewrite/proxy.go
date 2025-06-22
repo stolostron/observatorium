a67c7920f7da6cb2bf88eeb2b406db9bb0b017d2
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2025-06-22 06:27:27 +0000

    Update module github.com/go-chi/chi to v5
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- internal/remotewrite/proxy.go
+++ internal/remotewrite/proxy.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
